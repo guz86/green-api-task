97338613d7ea169f455248ef584cb9685035c948
Pavel Gordienko <guzmo@yandex.ru>
2025-02-04 02:21:29 +0300

    feat: add main template

---
+++ src/components/Chats/Chats.tsx
@@ -0,0 +1,5 @@
+import styles from './Chats.module.scss';
+
+export const Chats = () => {
+  return <div className={styles.container}>Chats</div>;
+};
---
+++ src/components/Dialog/Dialog.tsx
@@ -0,0 +1,5 @@
+import styles from './Dialog.module.scss';
+
+export const Dialog = () => {
+  return <div className={styles.container}>Dialog</div>;
+};
